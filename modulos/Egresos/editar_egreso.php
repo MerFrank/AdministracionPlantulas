70f21cb937913cb862c3353d784359e24cf07dab
DiegoM <villegasmisael15@gmail.com>
2025-09-26 13:56:44 -0600

    boton

--- modulos/Egresos/editar_egreso.php
+++ modulos/Egresos/editar_egreso.php
@@ -130,7 +130,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 $ruta = "dashboard_egresos.php";
-$texto_boton = "";
+$texto_boton = "Regresar";
 require __DIR__ . '/../../includes/header.php';
 ?>
 
