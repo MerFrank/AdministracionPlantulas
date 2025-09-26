c2ec8ddf6c458f7f853c09bbc220080fdee2525d
DiegoM <villegasmisael15@gmail.com>
2025-09-25 22:11:47 -0600

    cambio en el menu de opciones

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,7 @@ define('BASE_URL', '/AdministracionPlantulas');
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,12 +11,13 @@ define('BASE_URL', '/AdministracionPlantulas');
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   <link rel="stylesheet" href="/AdministracionPlantulas/assets/css/style.css">
-   <!-- Font Awesome -->
+  <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-  
+
   <!-- Bootstrap Icons -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
 </head>
+
 <body>
   <div class="contenedor-pagina">
     <header>
@@ -31,32 +33,35 @@ define('BASE_URL', '/AdministracionPlantulas');
 
       <div class="barra-navegacion">
         <nav class="navbar bg-body-tertiary">
-          <div class="container-fluid d-flex justify-content-between">
- 
-            <?php if (isset($opciones_menu)): ?>
-              <div class="dropdown">
-                <button class="save-button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  
-                  Opciones
-                </button>
-                <ul class="dropdown-menu">
-                  <?php foreach ($opciones_menu as $opcion): ?>
-                    <li><a class="dropdown-item" href="<?php echo $opcion['ruta']; ?>"><?php echo $opcion['texto']; ?></a></li>
-                  <?php endforeach; ?>
-                </ul>
-              </div>
-            <?php else: ?>
-              <div class="Opciones-barra">
-                <button class="save-button" onclick="window.location.href='<?php echo $ruta; ?>'">
-                  <i class="bi bi-arrow-left"></i> <?php echo $texto_boton; ?>
-                </button>
-              </div>
-            <?php endif; ?>
+          <div class="container-fluid d-flex justify-content-end">
+            <div class="d-flex gap-2">
 
+              <?php if (isset($opciones_menu)): ?>
+                <div class="dropdown">
+                  <button class="save-button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                    Opciones
+                  </button>
+                  <ul class="dropdown-menu">
+                    <?php foreach ($opciones_menu as $opcion): ?>
+                      <li><a class="dropdown-item" href="<?php echo $opcion['ruta']; ?>"><?php echo $opcion['texto']; ?></a></li>
+                    <?php endforeach; ?>
+                  </ul>
+                </div>
+              <?php endif; ?>
+
+              <?php if (isset($texto_boton)): ?>
+
+                <div class="Opciones-barra">
+                  <button class="save-button" onclick="window.location.href='<?php echo $ruta; ?>'">
+                    <i class="bi bi-arrow-left"></i> <?php echo $texto_boton; ?>
+                  </button>
+                </div>
+              <?php endif; ?>
+            </div>
           </div>
         </nav>
-     </div>
-  </header>
+      </div>
+    </header>
 </body>
 </html>
-
