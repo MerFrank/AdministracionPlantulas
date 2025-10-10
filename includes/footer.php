053f4984b603937a559e63232be3422a729ce1bc
DiegoM <villegasmisael15@gmail.com>
2025-10-10 10:56:20 -0600

    script para generar el pdf

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,16 @@
       <footer class="text-center mt-4">
         <p>&copy; <?php echo date('Y'); ?> PLANTAS AGRODEX. Todos los derechos reservados.</p>
       </footer>
-    </div> <!-- Cierre de .contenedor-pagina -->
+      </div> <!-- Cierre de .contenedor-pagina -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- <script src="/Plantulas/assets/js/main.js"></script> -->
-  </body>
-</html>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+      <!-- <script src="/Plantulas/assets/js/main.js"></script> -->
+      </body>
+
+      </html>
