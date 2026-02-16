b28fd76e8ace4cbc58a7faf1cce1e4815ee667c8
MerFrank <MeridaFranSiles@hotmail.com>
2026-02-16 11:43:06 -0600

    fix enlace dependencias

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,7 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/jspdf@latest/dist/jspdf.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js"></script>
       <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
