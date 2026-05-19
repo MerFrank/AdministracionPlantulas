c476ace1696ca636559a2ae312ad2fd3d2f2f856
DiegoM <villegasmisael15@gmail.com>
2026-05-19 11:01:12 -0600

    cambios en el footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,11 @@
-      <footer class="text-center mt-4">
+<footer class="text-center mt-4" style="margin-top: auto; flex-shrink: 0; width: 100%;">
         <p>&copy; <?php echo date('Y'); ?> PLANTAS AGRODEX. Todos los derechos reservados.</p>
       </footer>
-      </div> <!-- Cierre de .contenedor-pagina -->
+  </div> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/jspdf@latest/dist/jspdf.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/jspdf@latest/dist/jspdf.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js"></script>
-      <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-
-      <!-- <script src="/Plantulas/assets/js/main.js"></script> -->
-      </body>
-
-      </html>
+  </body>
+</html>
